205405aa70eb1bbdb4e5c6522d78e54aa6f71e50
Arturo Soberanes <arturo.soberanes01@gmail.com>
2022-09-22 11:12:47 -0500

    Add app

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,25 @@
 module.exports = (io) => {
-  console.log('Archivo de socket')
+  var data = []
+  var users = 0
+  io.on('connection', (socket) => {
+    data.forEach((draw) => {
+      io.emit('show_drawing', draw)
+    })
+    users = users + 1
+    io.emit('users', users)
+
+    socket.on('delete', () => {
+      data = []
+      io.emit('show_drawing', null)
+    })
+    socket.on('drawing', (drawing) => {
+      data.push(drawing)
+      io.emit('show_drawing', drawing)
+    })
+    socket.on('disconnect', () => {
+      users = users - 1
+      io.emit('users', users)
+    })
+
+  })
 }
